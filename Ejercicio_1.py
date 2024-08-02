b4836fa071a4f379f62c392884ed146f948a1958
Julii0422 <juliilopz0418@gmail.com>
2024-08-01 21:24:45 -0500

    Phyton

---
+++ Ejercicio_1.py
@@ -0,0 +1,8 @@
+
+print("Bienvenido usuario a esta sesion en linea ")
+username = input("Por favor, ingrese su nombre: ")
+age = input("Por favor , ingres su edad: ")
+print(f"Hola nombre:{username},edad:{age}")
+number = input("Por favor ingrese un numero entero : ")
+result = int(number) * 2
+print(result)
